6e65f7e4c302c70c5e7be7cd9389184881bc7f53
Dan Ossorio <daniel@ossorio.me>
2017-10-20 13:36:24 +0100

    App uses express now

--- app.js
+++ app.js
@@ -1,34 +1,31 @@
 import http from 'http';
 import express from 'express';
-import consolidate from 'consolidate';
 import mongodb from 'mongodb';
 import assert from 'assert';
 
-const PORT = 8000;
 const MongoClient = mongodb.MongoClient;
+const app = express();
+let db = null;
+const PORT = 8000;
 
-MongoClient.connect('mongodb://localhost:27017/test', (err, db) => {
-	assert.equal(null, err);
-	console.log('Connected to mongodb');
-	startHttpServer(db);
+app.get('/', (req, res) => {
+	res.writeHead(200, {'content-type': 'text/plain'});
+
+	db.collection('names').find({}).toArray()
+		.then((names) => {
+			names.forEach((name) => {
+				res.write(`Name: ${name.name}\n`);
+			});
+			res.end('\nThat was all!');
+		});
 });
 
-function startHttpServer(db) {
-	let httpServer = http.createServer((req, res) => {
-		res.writeHead(200, {'content-type': 'text/plain'});
+MongoClient.connect('mongodb://localhost:27017/test', (err, dbConn) => {
+	assert.equal(null, err);
 
-		db.collection('names').find({}).toArray()
-			.then((names) => {
-				names.forEach((name) => {
-					res.write(`Name: ${name.name}\n`);
-				});
+	db = dbConn;
+	console.log('Connected to mongodb');
 
-				res.end('\nThat was all!');
-			}
-		);
-	});
-
-	httpServer.listen(PORT);
-
+	app.listen(PORT);
 	console.log(`Server running at http://localhost:${PORT}`);
-}
+});
